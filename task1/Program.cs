27ec98481b8b8f3833486b86f590518c1d12e70e
LarsonRagnar <larionov1992@gmail.com>
2023-02-26 17:21:04 +0300

    пример вызова метода

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// void: нет оператора return   называют процедурой
+// тип_возвращаемого_значения имя (Параметры)
+
+int Calculate(int a, int b, char sign)
+{
+    int result = 0;
+    if (sign == '*')
+    {
+        result = a * b;
+    }
+    else if (sign == '+')
+    {
+        result = a + b;
+    }
+    else if (sign == '-')
+    {
+        result = a - b;
+    }
+    else
+    {
+        Console.WriteLine("такого знака нет");
+    }
+    return result;                               // ПРИМЕР ВЫЗОВА МЕТОДА
+} 
+Console.WriteLine(Calculate(3,5,'*'));
